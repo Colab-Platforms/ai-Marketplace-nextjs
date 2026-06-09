35c27b9c77b867e00bedd2d8cc58e9b5c24c6e8a
tech975 <tech@colabplatforms.com>
2026-06-09 13:50:07 +0530

    fixed api for next

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = process.env.API_BASE_URL?? 'http://localhost:5000';
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000';
+
+console.log('Base URL:', baseURL);
 
 const apiClient = axios.create({
   baseURL,
